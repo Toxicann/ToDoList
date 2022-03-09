35ce7a92ff2dab601488f963dfd72786ed403790
Toxicann <anushkarki007@gmail.com>
2022-03-09 13:35:43 +0545

    general layout being made

---
+++ lib/screens/task_screens.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class TasksScreens extends StatelessWidget {
+  const TasksScreens({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(
+          Icons.add,
+          size: 25.0,
+        ),
+        onPressed: () {},
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60.0,
+              left: 30.0,
+              right: 30.0,
+              bottom: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 50.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 40.0,
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  "My TODOS",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "5 Left",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
